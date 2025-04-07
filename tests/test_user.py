0c0bc0c7535c98bd049a1b48e5211d2754066c61
vest1ter <geninkirill526@gmail.com>
2025-04-07 11:37:06 +0300

    test4

--- tests/test_user.py
+++ tests/test_user.py
@@ -26,16 +26,24 @@ def test_get_existed_user():
 
 def test_get_unexisted_user():
     '''Получение несуществующего пользователя'''
-    pass
+    response = client.get("/api/v1/user", params={'email': 'lolikekov@gmail.com'})
+    assert response.status_code == 404
+
 
 def test_create_user_with_valid_email():
     '''Создание пользователя с уникальной почтой'''
-    pass
+    response = client.post("/api/v1/user", json={'name': 'adminchik', 'email': 'aadmin@gmail.com'})
+    assert response.status_code == 201
 
 def test_create_user_with_invalid_email():
     '''Создание пользователя с почтой, которую использует другой пользователь'''
-    pass
+    response = client.post("/api/v1/user", json={'name': 'used', 'email': 'i.i.ivanov@mail.com'})
+
+    assert response.status_code == 409
+    assert response.json()['detail'] == "User with this email already exists"
 
 def test_delete_user():
     '''Удаление пользователя'''
-    pass
+    response = client.delete("/api/v1/user", params={'email': 'i.i.ivanov@mail.com'})
+
+    assert response.status_code == 200
